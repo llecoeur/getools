1aaedb4a9a244e735660a27bac9aa825c2a257b5
llecoeur <informatique@manuplast.fr>
2021-02-11 11:44:56 +0100

    1er version imprimé adhérents

--- activite/views.py
+++ activite/views.py
@@ -808,50 +808,42 @@ def download_releve_adherent(request, mois, annee):
     # adherent_list = Adherent.objects.all().order_by("raison_sociale")
     # adherent_list = Adherent.objects.exclude(raison_sociale="PROGRESSIS").filter(raison_sociale__in=["MANUPLAST"])
     adherent_list = Adherent.objects.exclude(raison_sociale="PROGRESSIS").order_by("raison_sociale")
-    for adherent in tqdm(adherent_list):
+    for adherent in adherent_list:
         # liste des mises a dispo de l'adhérent
         mad_list = adherent.mise_a_disposition_list.filter(cloturee=False)
         for mad in mad_list:
             
-            mad_dict = model_to_dict(mad)
-            # releve
-            # récupérétion du relevé du mois, et création si il n'existe pas
-            try:
-                releve = ReleveSalarie.objects.get(salarie=mad.salarie, annee=annee, mois=mois)
-            except ReleveSalarie.DoesNotExist:
-                releve = ReleveSalarie()
-                releve.salarie = mad.salarie
-                releve.mois = mois
-                releve.annee = annee
-                releve.save()
-            
-            # mad_dict['releve'] = model_to_dict(releve)
+            # On regarde si il y a des saisies pour cette mad sur le mois
+            saisie_count = SaisieActivite.objects.filter(tarif__mise_a_disposition=mad).filter(date_realisation__month=mois, date_realisation__year=annee).count()
+            if saisie_count != 0:
+                mad_dict = model_to_dict(mad)
+                # releve
+                # récupérétion du relevé du mois, et création si il n'existe pas
 
-            # salarie = Salarie.objects.get(pk=salarie_id)
-            salarie_dict = model_to_dict(mad.salarie)
-            mad_dict['salarie'] = salarie_dict
-            mad_dict['adherent'] = model_to_dict(adherent)  
-            # Infos supplémentaires des salariés
-            salarie_info_sup = mad.salarie.get_info_sup(mois, annee)
-            # mad_dict['salarie']['infos_sup'] = model_to_dict(salarie_info_sup)
-            # mad_dict['salarie']['infos_sup']['heures_travaillees'] = salarie_info_sup.heures_travaillees
+                # salarie = Salarie.objects.get(pk=salarie_id)
+                salarie_dict = model_to_dict(mad.salarie)
+                mad_dict['salarie'] = salarie_dict
+                mad_dict['adherent'] = model_to_dict(adherent)  
+                # Infos supplémentaires des salariés
+                # salarie_info_sup = mad.salarie.get_info_sup(mois, annee)
+                # mad_dict['salarie']['infos_sup'] = model_to_dict(salarie_info_sup)
+                # mad_dict['salarie']['infos_sup']['heures_travaillees'] = salarie_info_sup.heures_travaillees
 
-            # Liste des tarifs
-            mad_dict['tarifs_ge'] = mad.tarif_ge_list_dict
+                # Liste des tarifs
+                mad_dict['tarifs_ge'] = mad.tarif_ge_list_dict
 
-            # Primes Fofaitaires
-            # mad_dict['primes_forfaitaires'] = mad.tarifs_ge_prime_forfaitaire_dict()
+                # Primes Fofaitaires
+                # mad_dict['primes_forfaitaires'] = mad.tarifs_ge_prime_forfaitaire_dict()
 
-            # Liste des jours, des saisies, etc, pour construire le tableau de saisie
-            mad_dict['jour_list'] = mad.get_saisies_from_mois_dict_all(mois, annee)
+                # Liste des jours, des saisies, etc, pour construire le tableau de saisie
+                mad_dict['jour_list'] = mad.get_saisies_from_mois_dict_all(mois, annee)
 
-            # informations supplémentaires des mises a disposition
-            # mad_dict['infos_sup'] = model_to_dict(mad.get_info_sup(mois, annee))
+                # informations supplémentaires des mises a disposition
+                # mad_dict['infos_sup'] = model_to_dict(mad.get_info_sup(mois, annee))
 
-            # Liste des primes forfaitaires enregistrées
-            # mad_dict['prime_forfaitaires_values'] = mad.prime_forfaitaire_values_list(annee, mois)
-            ret.append(mad_dict)
-            pprint(mad_dict)
+                # Liste des primes forfaitaires enregistrées
+                # mad_dict['prime_forfaitaires_values'] = mad.prime_forfaitaire_values_list(annee, mois)
+                ret.append(mad_dict)
 
     context = {
         "mad_list": ret,

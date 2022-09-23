880c42699291ca03782698bbe3d335ddf855460d
llecoeur <informatique@manuplast.fr>
2022-09-23 15:15:59 +0200

    Ajout d'une description

--- config/models.py
+++ config/models.py
@@ -8,6 +8,7 @@ class Config(models.Model):
     key = models.CharField("Clef", max_length=20, null=False, blank=False)
     str_val = models.CharField("Valeur Chaine", max_length=100, null=True, blank=True)
     int_val = models.IntegerField("Valeur Entière", null=True, blank=True)
+    description = models.CharField("description", max_length=200, null=False, blank=False, default="")
 
     def __str__(self):
-        return f"{self.key}"
+        return f"{self.description}"
